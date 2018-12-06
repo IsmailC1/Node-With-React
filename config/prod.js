8756d344a0d8988ee0243cc788ad9a83515a9684
Ismail Cucevic <cucevicismail@gmail.com>
2018-12-06 19:42:53 +0100

    finished auth flow

---
+++ config/prod.js
@@ -0,0 +1,10 @@
+//prod.js - production keys!!
+module.exports = {
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: proccess.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: proccess.env.MONGO_URI,
+  cookieKey: proccess.env.COOKIE_KEY
+};
+
+
+mongodb://stevenProd:password2@ds261078.mlab.com:61078/emaily-prod
