b8c5cb9f07e88afa6ac2a78169991a8eb2889a00
Ismail Cucevic <cucevicismail@gmail.com>
2018-12-14 21:00:34 +0100

    save channges

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import Payments from "./Payments";
 
 class Header extends Component {
   renderContent() {
-    switch (this.props.auth) {
+    switch (this.props.auth.auth) {
       case null:
         return;
       case false:
@@ -13,11 +15,17 @@ class Header extends Component {
           </li>
         );
       default:
-        return (
-          <li>
-            <a>Logout</a>
+        return [
+          <li key="1">
+            <Payments />
+          </li>,
+          <li key="3" style={{ margin: "0 10px" }}>
+            Credits: {this.props.auth.auth.credits}
+          </li>,
+          <li key="2">
+            <a href="/api/logout">Logout</a>
           </li>
-        );
+        ];
     }
   }
 
@@ -26,9 +34,12 @@ class Header extends Component {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a href="www.google.com" className="left brand-logo">
+          <Link
+            to={this.props.auth ? "/surveys" : "/"}
+            className="left brand-logo"
+          >
             Emaily
-          </a>
+          </Link>
           <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
